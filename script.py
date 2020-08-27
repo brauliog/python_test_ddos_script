ee930053d0ac87ca8fcdb1ffb9825c89e0065653
Braulio Gonzalez <braulio.gonzalez@braulios-air.lan>
2020-08-26 21:35:25 -0700

    main script

---
+++ script.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import attack_service
+
+# ask for target ip and port number
+t_ip = attack_service.target_ip()
+port_num = attack_service.set_port()
+
+
+
+
